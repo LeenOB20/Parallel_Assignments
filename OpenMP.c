0275cc6c7be7246ec6d4676257c1ff6c989af687
LeenOB20 <132571161+LeenOB20@users.noreply.github.com>
2023-11-11 19:58:45 +0200

    Update OpenMP.c

--- OpenMP.c
+++ OpenMP.c
@@ -1,36 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <omp.h>
-
-#define num_threads 8
-
-int main() {
-    int i, j, k;
-    double a[num_threads][num_threads], b[num_threads][num_threads], c[num_threads][num_threads];
-
-    for (i = 0; i < num_threads; ++i) {
-        for (j = 0; j < num_threads; ++j) {
-            a[i][j] = i + j;
-            b[i][j] = i - j;
-            c[i][j] = 0;
+#define MAX_THREADS 4
+int N, M, P;
+double **A, **B, **C;
+int main(int argc, char *argv[]) {
+    N = atoi(argv[1]);
+    M = atoi(argv[2]);
+    P = atoi(argv[3]);
+    A = (double**) malloc(N * sizeof(double*));
+    B = (double**) malloc(M * sizeof(double*));
+    C = (double**) malloc(N * sizeof(double*));
+    for (int i = 0; i < N; i++) {
+        A[i] = (double*) malloc(M * sizeof(double));
+        C[i] = (double*) malloc(P * sizeof(double));
+        for (int j = 0; j < M; j++) {
+            A[i][j] = rand() % 10;
         }
     }
-
-    double start_time = omp_get_wtime();
-
-    #pragma omp parallel for private(i,j,k) shared(a,b,c) schedule(dynamic) 
-    {
-        for (i = 0; i < num_threads; ++i) {
-            for (j = 0; j < num_threads; ++j) {
-                for (k = 0; k < num_threads; ++k) {
-                    c[i][j] += a[i][k] * b[k][j];
-                }
+    for (int i = 0; i < M; i++) {
+        B[i] = (double*) malloc(P * sizeof(double));
+        for (int j = 0; j < P; j++) {
+            B[i][j] = rand() % 10;
+        }
+    }
+    struct timeval start_time, end_time;
+    gettimeofday(&start_time, NULL);
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < P; j++) {
+            double sum = 0;
+            for (int k = 0; k < M; k++) {
+                sum += A[i][k] * B[k][j];
             }
+            C[i][j] = sum;
         }
     }
-
-    double end_time = omp_get_wtime();
-    printf("openMP CODE TIME TOOK: %f seconds\n", end_time - start_time);
-
+    gettimeofday(&end_time, NULL);
+    double elapsed_time = (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) / 1e6;
+    double flops = 2 * N * M * P;
+    double mflops = flops / (elapsed_time * 1e6);
+    double speedup = mflops / (N * M * P / elapsed_time);
+    double efficiency = speedup / MAX_THREADS;
+    double scalability = speedup / (N * M * P / elapsed_time / MAX_THREADS);
+    printf("Elapsed time (s): %f\n", elapsed_time);
+    printf("Speedup: %f\n", speedup);
+    printf("Efficiency: %f\n", efficiency);
+    printf("Scalability: %f\n", scalability);
+    for (int i = 0; i < N; i++) {
+        free(A[i]);
+        free(C[i]);
+    }
+    for (int i = 0; i < M; i++) {
+        free(B[i]);
+    }
+    free(A);
+    free(B);
+    free(C);
     return 0;
 }
